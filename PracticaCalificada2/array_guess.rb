b5f2ae77e104fca77e01a62d3ad65c1670416f66
Jaime Gonzalo Acuña Napan <92898224+peg1163@users.noreply.github.com>
2023-10-11 10:27:50 -0500

    Update array_guess.rb

--- PracticaCalificada2/array_guess.rb
+++ PracticaCalificada2/array_guess.rb
@@ -1,14 +1,14 @@
-
 def letra_guess(frase)
-  #inicializaremos un arreglo con el alfabeto
-  alfabeto = ('a'..'z').to_a
-  #iteraremos sobre la frase y borraremos las letras que se encuentren en el arreglo
-  frase.split('').each do |letra|
-    alfabeto.delete(letra)
-  end
-  alfabeto.first
+    #definiremos un arreglo con un alfabeto 
+    alfabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+        'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+        'u', 'v', 'w', 'x', 'y', 'z']
+    #recorreremos la frase y eliminaremos las letras que esten en el alfabeto y en la frase
+    frase.each_char do |letra|
+        alfabeto.delete(letra)
+    end
+    #finalmente devolveremos las letras del alfabeto que no esten en la frase 
+    return alfabeto
 end
-
-
-frase = "the quick brown fox jumps over the lazy dog"
+frase="the quick brown box jumps over a lazy dog "
 puts letra_guess(frase)
