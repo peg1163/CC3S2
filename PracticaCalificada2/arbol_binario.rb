32ba72075d81ec0023bf45316420e6ee757d9ee7
Jaime Gonzalo Acuña Napan <jaime.acuna.n@uni.pe>
2023-10-11 12:56:17 -0500

    update

--- PracticaCalificada2/arbol_binario.rb
+++ PracticaCalificada2/arbol_binario.rb
@@ -1,8 +1,17 @@
-class nodo
-  @hijo_izquierdo
-  @hijo_derecho
+class BinaryTree
+  #se le da acceso a los atributos de la clase
+  attr_accessor :hijo_izquierdo, :hijo_derecho , :valor
 
   def initialize(valor)
+    #definimos los atributos de la clase
+    @valor = valor
+    @hijo_izquierdo=:null
+    @hijo_derecho=:null
+  end
+  def
 
+  end
+  def empty? #este metodo devolvera true si el arbol esta vacio
+    return @valor.nil? &&  @hijo_izquierdo.nil? && @hijo_derecho.nil?
   end
 end
