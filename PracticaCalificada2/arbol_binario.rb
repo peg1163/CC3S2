7286c81bcb2a1cd832890baf7dc1c3918013296d
Jaime Gonzalo Acuña Napan <jaime.acuna.n@uni.pe>
2023-10-11 17:41:25 -0500

    update

--- PracticaCalificada2/arbol_binario.rb
+++ PracticaCalificada2/arbol_binario.rb
@@ -8,10 +8,29 @@ class BinaryTree
     @hijo_izquierdo=:null
     @hijo_derecho=:null
   end
-  def
+  #Usaremos este metodo para poder insertar un elemento en el arbol
+  #comparara si es menor o mayor que el valor del nodo
+  #y lo posicionara a la izquierda o derecha
+  def pos(elemento)
+    if  elemento <=value
+        if @hijo_izquierdo.nil?
+            @hijo_izquierdo = BinaryTree.new(elemento)
+        else
+            @hijo_izquierdo.pos(elemento)
+        end
+    else
+        if @hijo_derecho.nil?
+            @hijo_derecho = BinaryTree.new(elemento)
+        else
+            @hijo_derecho.pos(elemento)
+        end
+    end
 
   end
   def empty? #este metodo devolvera true si el arbol esta vacio
-    return @valor.nil? &&  @hijo_izquierdo.nil? && @hijo_derecho.nil?
+    return @hijo_izquierdo.nil? && @hijo_derecho.nil?
   end
+end
+def each
+
 end
